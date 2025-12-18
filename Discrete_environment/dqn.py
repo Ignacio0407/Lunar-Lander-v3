bbe4df156d12ea8f50e26abfcb5aecd9403559a6
Ignacio0407 <igngutser@alum.us.es>
2025-12-18 13:50:57 +0100

    Forgot to upload this before

--- Discrete_environment/dqn.py
+++ Discrete_environment/dqn.py
@@ -13,6 +13,20 @@ class DQN(nn.Module):
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
         return self.layer3(x)
+    
+class DQN2(nn.Module):
+    def __init__(self, n_observations:int, n_actions:int):
+        super().__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, 128)
+        self.layer4 = nn.Linear(128, n_actions)
+
+    def forward(self, x:Tensor):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = F.relu(self.layer3(x))
+        return self.layer4(x)
 
 class DQN_heavy(nn.Module):
     def __init__(self, n_observations: int, n_actions: int, dropout_p: float = 0.1):
